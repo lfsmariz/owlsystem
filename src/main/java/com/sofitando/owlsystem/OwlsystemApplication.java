646235567e723f4d181fc1d263d78abc59400233
Luiz Mariz <lfsmariz@gmail.com>
2021-04-15 11:11:58 -0300

    first commit

---
+++ src/main/java/com/sofitando/owlsystem/OwlsystemApplication.java
@@ -0,0 +1,13 @@
+package com.sofitando.owlsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwlsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OwlsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sofitando/owlsystem/OwlsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofitando.owlsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwlsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
